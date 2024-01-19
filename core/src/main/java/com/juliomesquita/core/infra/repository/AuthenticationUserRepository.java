63994e0922263968784f77347ad65ce713c71a9f
Julio C. Mesquita <juliocesarmcamilo@gmail.com>
2024-01-19 00:18:44 -0300

    configurando arquivo modularizado

--- core/src/main/java/com/juliomesquita/core/infra/repository/AuthenticationUserRepository.java
+++ core/src/main/java/com/juliomesquita/core/infra/repository/AuthenticationUserRepository.java
@@ -1,11 +1,13 @@
 package com.juliomesquita.core.infra.repository;
 
 import com.juliomesquita.core.domain.model.entities.AuthenticationUser;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface AuthenticationUserRepository extends JpaRepository<AuthenticationUser, Long> {
+@Repository
+public interface AuthenticationUserRepository extends PagingAndSortingRepository<AuthenticationUser, Long> {
     AuthenticationUser findByUsername(String username);
     Optional<AuthenticationUser> findByEmail(String email);
 }
